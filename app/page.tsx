65ed6616774997975cd53115e6acf36610b18dc4
Davi <davi.edfisica@outlook.com>
2025-01-30 19:03:31 +0000

    criado o container

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 export default function Home() {
   return (
+     <div className="container mx-auto p-4">
     <div className="centered bg-gradient shadow">
+      <img
+      src="jpg"
+      alt="Descrição da foto "
+      className="foto"
+      />
       <h1 className="text-4xl font-bold mb-4">Aqui é a primeira página</h1>
-      <p className="text-lg">Bem-vindo ao nosso site! 🌟</p>
+      <p className="text-lg">Bem-vindo ao nosso site!</p>
+    </div>
     </div>
   );
 }
