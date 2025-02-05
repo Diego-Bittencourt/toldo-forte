f2aa36338a545b8b66669823798b7bc42655178a
Davi <davi.edfisica@outlook.com>
2025-02-05 13:26:15 +0000

    logo e links separados

--- app/components/organisms/Header.tsx
+++ app/components/organisms/Header.tsx
@@ -4,16 +4,26 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" className="appbar shadow">
-        <Toolbar>
+      <AppBar position="static" className="appbar shadow" sx={{ backgroundColor: '#1976d2' }}>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
           <div className="logo">
-            <Typography variant="h6" component="div">
+            <Typography variant="h6" component="div" sx={{ fontWeight: 'bold' }}>
               Toldo Forte
             </Typography>
           </div>
           <div className="nav-links">
-            <Link href="/sobre">Sobre</Link>
-            <Link href="/contato">Contato</Link>
+            <Link href="/sobre" passHref>
+              <Typography variant="body1" component="span" 
+              sx={{ marginLeft: 10, color: 'white', textDecoration: 'none', '&:hover': { textDecoration: 'underline' } }}>
+                Sobre
+              </Typography>
+            </Link>
+            <Link href="/contato" passHref>
+              <Typography variant="body1" component="span" 
+              sx={{ marginLeft: 10, color: 'white', textDecoration: 'none', '&:hover': { textDecoration: 'underline' } }}>
+                Contato
+              </Typography>
+            </Link>
           </div>
         </Toolbar>
       </AppBar>
