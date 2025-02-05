e4a31a73c66e0641adf5d8ad00771eb1c983b7d3
Davi <davi.edfisica@outlook.com>
2025-02-05 13:30:37 +0000

    necessario usar logotipo da empresa

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
-     <div className="container mx-auto p-4">
+     <div className="container mx-auto p-">
     <div className="centered bg-gradient shadow">
       <img
-      src="jpg"
-      alt="Descrição da foto "
-      className="foto"
+      src="/fake.png"
+      alt="logotipo"
+      className="mb-8"
       />
       <h1 className="text-4xl font-bold mb-4">Aqui é a primeira página</h1>
       <p className="text-lg">Bem-vindo ao nosso site!</p>
